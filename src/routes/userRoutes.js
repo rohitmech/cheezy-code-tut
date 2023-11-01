e313f9cfde4b702d9107defe7cbfaf097b78173f
Rohit Singh <rohitmech8003@gmail.com>
2023-11-01 19:14:09 +0530

    Added build command

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { signup, signin } = require("../Controllers/userController.js");
+const userController = require("../Controllers/userController.js");
 const userRouter = express.Router();
 
-userRouter.post("/signup", signup);
+userRouter.post("/signup", userController.signup);
 
-userRouter.post("/signin", signin);
+userRouter.post("/signin", userController.signin);
 
 module.exports = userRouter;
